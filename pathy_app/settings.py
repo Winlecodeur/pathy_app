cead9532d71c48c6ea770aef76fabde6a111747a
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-12-27 22:21:16 +0200

    Update settings.py

--- pathy_app/settings.py
+++ pathy_app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a$f$lb!&vsg2=1==@&1kc46ru1#nph6n9xij6r2oqz^y=_zf77'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
